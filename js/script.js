dd62484f30cb92e49b43b739ebfa6ae82a9c63cb
delfina <delfina.ghimire@gmail.com>
2019-07-03 14:58:24 +0545

    initial commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  //toggler-btn
+  $(".navbar-toggler").click(function() {
+    $(".navbar-toggler").toggleClass("change");
+  });
+
+  //sticky navbar and less padding
+  $(window).scroll(function() {
+    let position = $(this).scrollTop();
+    if (position >= 718) {
+      $(".navbar").addClass("navbar-background");
+      $(".navbar").addClass("fixed-top");
+    } else {
+      $(".navbar").removeClass("navbar-background");
+      $(".navbar").removeClass("fixed-top");
+    }
+  });
+
+  //smooth scroll
+  $(".nav-item a").click(function() {
+    link.preventDefault();
+    let target = $(this).attr("href");
+    $("html, body").animate(
+      {
+        scrollTop: $(target).offset().top - 25
+      },
+      3000
+    );
+  });
+
+  //ripples
+  $("header, .nav-sub").ripples({
+    dropRadius: 10,
+    perturbance: 0,
+    resolution: 512
+  });
+
+  //magnific popup
+  $(".parent-container").magnificPopup({
+    delegate: "a", // child items selector, by clicking on it popup will open
+    type: "image",
+    // gallery
+    gallery: {
+      enabled: true
+    }
+  });
+});
